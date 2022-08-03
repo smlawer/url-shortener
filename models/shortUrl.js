a5bb11feed9734d1c5f0ef23b260ef42fb15b982
smlawer <smlawer@gmail.com>
2022-08-02 20:54:44 -0400

    updated comments

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,21 +1,26 @@
 const mongoose = require("mongoose");
 const shortId = require("shortid");
 
+// MongoDB schema for URLS
 const shortUrlSchema = new mongoose.Schema({
+  // full URL entered by user
   full: {
     type: String,
     required: true,
   },
+  // Short URL hashed by shortID
   short: {
     type: String,
     required: true,
     default: shortId.generate,
   },
+  // Number of clicks on shortened URL
   clicks: {
     type: Number,
     required: true,
     default: 0,
   },
+  // Date that shortURL was generated
   date: {
     type: String,
     required: true,
--- server.js
+++ server.js
@@ -9,19 +9,23 @@ mongoose.connect("mongodb://localhost/urlShortener", {
   useUnifiedTopology: true,
 });
 
+// Use EJS
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
+// Get all shortURLS and send to index.ejs
 app.get("/", async (req, res) => {
   const shortUrls = await ShortUrl.find();
   res.render("index", { shortUrls: shortUrls });
 });
 
+// Create new short url based on entered URL and reload page
 app.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
   res.redirect("/");
 });
 
+// Redirect from short URL to full URL
 app.get("/:shortUrl", async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
 
